3570abdc6ec0023fabef9c88fcc139827bf274d7
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-07-16 13:57:13 +0900

    fix
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ echo '::endgroup::'
 # Install extra python modules
 echo '::group:: Installing extra python modules'
 if [[ "x${EXTRA_REQUIREMENTS_TXT}" != "x" ]]; then
-	uv pip install --no-cache-dir -r "${EXTRA_REQUIREMENTS_TXT}" --use-deprecated=legacy-resolver
+	uv pip install --no-cache-dir -r "${EXTRA_REQUIREMENTS_TXT}"
 	# Make sure the installed modules
 	uv pip list
 fi
